166e8ed598bf1816b17e04990c09c059c5029c93
Osamudiamen <osas.azamegbe@gmail.com>
2022-02-05 01:35:44 +0000

    add lottery functionality to contract

--- contract/Lottery.sol
+++ contract/Lottery.sol
@@ -2,9 +2,33 @@
 pragma solidity ^0.8.11;
 
 contract Lottery {
-	address public m_manager;
-	
-	constructor() {
-		m_manager = msg.sender;
-	}
+    address public m_manager;
+    address payable[] public m_players;
+
+    constructor() {
+        m_manager = msg.sender;
+    }
+
+    function enterLottery() public payable {
+        require(msg.value >= 0.01 ether);
+        m_players.push(payable(msg.sender));
+    }
+
+    //psuedo-random uint gen.
+    function random() private view returns (uint256) {
+        return
+            uint256(
+                keccak256(
+                    abi.encode(block.difficulty, block.timestamp, m_players)
+                )
+            );
+    }
+
+    function pickWinner() public {
+        require(msg.sender == m_manager);
+
+        uint256 index = random() % m_players.length;
+        m_players[index].transfer(address(this).balance);
+        m_players = new address payable[](0);
+    }
 }
